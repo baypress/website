930f5cbe5d34709baaf2cf6b466e447071c144c6
Brian Kang <brianlkang@gmail.com>
2021-05-06 10:00:07 -0700

    hover state for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Link } from 'gatsby';
 import { StaticImage } from "gatsby-plugin-image";
 import styled from 'styled-components';
@@ -17,39 +17,62 @@ const LogoAndContact = styled('div')`
 `;
 
 const NavBar = styled('div')`
-
 `;
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Spacing>
-      <LogoAndContact>
-        <Link to="/">
-          <StaticImage src={'../images/icon.svg'} width={61} height={42} formats={['AUTO', 'WEBP', 'AVIF', 'SVG']} />
-        </Link>
-        <Link to="/contact">
-          <label>Contact us</label>
-        </Link>
-      </LogoAndContact>
-      <NavBar>
-        <Link to="/custom">
-          <button>Custom Packaging & Print</button>
-        </Link>
-        <Link to="/industrial">
-          <button>Industrial Packaging</button>
-        </Link>
-        <Link to="/supplies">
-          <button>Shipping Supplies</button>
-        </Link>
-        <Link to="/services">
-          <button>Services</button>
-        </Link>
-        <Link to="/about">
-          <button>About Us</button>
-        </Link>
-      </NavBar>
-    </Spacing>
-  </header >
-)
+const NavButton = styled('button')`
+  background-color: white;
+  border: none;
+  cursor: pointer;
+  color: ${({ selected }) => selected ? '#6e26ec' : '#4b463e'};
+
+  &:hover {
+    color: #6e26ec;
+  }
+`;
+
+const paths = {
+  contact: '/contact',
+  custom: '/custom',
+  industrial: '/industrial',
+  supplies: '/supplies',
+  services: '/services',
+  about: '/about',
+}
+
+const Header = ({ siteTitle }) => {
+  const [currentPath, setPath] = useState(window.location.pathname);
+
+  return (
+    <header>
+      <Spacing>
+        <LogoAndContact>
+          <Link to="/">
+            <StaticImage src={'../images/icon.svg'} width={61} height={42} formats={['AUTO', 'WEBP', 'AVIF', 'SVG']} />
+          </Link>
+          <Link to={paths.contact}>
+            <button>Contact us</button>
+          </Link>
+        </LogoAndContact>
+        <NavBar>
+          <Link to={paths.custom}>
+            <NavButton selected={currentPath === paths.custom}>Custom Packaging & Print</NavButton>
+          </Link>
+          <Link to={paths.industrial}>
+            <NavButton selected={currentPath === paths.industrial}>Industrial Packaging</NavButton>
+          </Link>
+          <Link to={paths.supplies}>
+            <NavButton selected={currentPath === paths.supplies}>Shipping Supplies</NavButton>
+          </Link>
+          <Link to={paths.services}>
+            <NavButton selected={currentPath === paths.services}>Services</NavButton>
+          </Link>
+          <Link to={paths.about}>
+            <NavButton selected={currentPath === paths.about}>About Us</NavButton>
+          </Link>
+        </NavBar>
+      </Spacing>
+    </header >
+  );
+};
 
 export default Header
